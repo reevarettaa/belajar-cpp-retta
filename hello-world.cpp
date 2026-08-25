8d8eabfcda3d031428f30f955c894531b2578bab
reevarettaa <elvarettaevanaws5@gmail.com>
2026-08-25 08:40:46 +0700

    first project

---
+++ hello-world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = a << 2;
+    cout << "Hello, World!" << endl;
+    cout << "Ini Nilai A = " << a << endl;
+    cout << "Ini Nilai B = " << b << endl;
+    return 0;
+}
