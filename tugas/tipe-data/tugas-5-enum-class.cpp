2f9d720fd00a80a4de00c84fba385ebd67052082
reevarettaa <elvarettaevanaws5@gmail.com>
2026-09-20 20:11:56 +0700

    tugas tipe data 5

--- tugas/tipe-data/tugas-5-enum-class.cpp
+++ tugas/tipe-data/tugas-5-enum-class.cpp
@@ -2,6 +2,51 @@
 
 using namespace std;
 
-int main(){
+enum StatusPembayaran {
+    BELUM_BAYAR,
+    LUNAS,
+    MENUNGGAK
+};
 
+class Siswa {
+public:
+    string nama;
+    StatusPembayaran status;
+
+    Siswa(string n, StatusPembayaran s) {
+        nama = n;
+        status = s;
+    }
+
+    void tampilkanStatus() {
+        cout << "Nama   : " << nama << endl;
+        cout << "Status : ";
+
+        switch (status) {
+            case BELUM_BAYAR:
+                cout << "Belum Bayar" << endl;
+                break;
+            case LUNAS:
+                cout << "Lunas" << endl;
+                break;
+            case MENUNGGAK:
+                cout << "Menunggak" << endl;
+                break;
+        }
+        cout << "-----------------------------------" << endl;
+    }
+};
+
+int main(){
+    Siswa siswa1("Ella", LUNAS);
+    Siswa siswa2("Rhea", BELUM_BAYAR);
+    Siswa siswa3("Eva", MENUNGGAK);
+
+    cout << "=== Status Pembayaran Siswa ===" << endl;
+
+    siswa1.tampilkanStatus();
+    siswa2.tampilkanStatus();
+    siswa3.tampilkanStatus();
+
+    return 0;
 }
